c9265d02c35dc764359ed5305b400fd2207341c7
namnv <namnv@pitagon.vn>
2022-08-02 15:32:36 +0700

    update

---
+++ aspnet/AspNetFrist/Note.cs
@@ -0,0 +1,22 @@
+﻿namespace AspNetFrist
+{
+    /* chỉ tạo ra class này để ghi chú */
+    public class Note
+    {
+        /*
+        Run project
+            - khi chạy với tên project tức là đang chạy sử dụng Dotnet CLI
+            - khi chạy với tên IIS Express thì đang chạy theo cách IIS Express.
+            
+        Cấu trúc thư mục
+            wwwroot:
+                - chứa các file tĩnh như html, css, js và có thể tự tạo ra.
+            dependencies:
+                - thư mục chứa các thành phần phụ thuộc như các package, ...
+
+
+
+
+        */
+    }
+}
